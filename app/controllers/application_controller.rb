ee04d0290dd7f0d7bd29da26282010e40fea0d27
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-06-28 21:30:00 -0400

    Added Username to users, created basic userprofile page and linked the user thumbnail pictures to the profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    registration_params = [:genre, :new_release, :avatar, :email, :password, :password_confirmation]
+    registration_params = [:favorite_movie, :username, :biography, :avatar, :email, :password, :password_confirmation]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) { 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :users
   
   get '/action', to: 'movies#action'
   get '/animated', to: 'movies#animated'
